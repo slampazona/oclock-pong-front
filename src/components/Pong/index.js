17cd2db0621f163de3308f6c64c3cc74a4871019
Sébastien Lampazona <sebastien@oclock.io>
2021-11-24 23:35:53 +0100

    Pose de la base

---
+++ src/components/Pong/index.js
@@ -0,0 +1,12 @@
+// == Import
+import './styles.scss';
+
+// == Composant
+const Pong = () => (
+  <div className="pong">
+    <h1>O'Pong</h1>
+  </div>
+);
+
+// == Export
+export default Pong;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// == Import : npm
+import { render } from 'react-dom';
+
+// == Import : local
+import Pong from 'src/components/Pong';
+
+render(<Pong />, document.getElementById('root'));
